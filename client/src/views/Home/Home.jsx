b62b71f095243cfabbaa4af6388a1234e78a1230
ajpintos <ajpintos@gmail.com>
2023-04-17 00:44:02 -0300

    spinner

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -4,6 +4,7 @@ import {useDispatch} from "react-redux";
 import {getCountries} from "../../redux/actions";
 import NavBar from "../../components/NavBar/NavBar";
 import App from "../../App";
+import {ClipLoader} from "react-spinners";
 const CardsContainer = lazy(() => import('../../components/CardsContainer/CardsContainer'));
 
 const Home = () => {
@@ -12,11 +13,30 @@ const Home = () => {
     useEffect(()=>{
         dispatch(getCountries());
     },[dispatch])
+
+    //! Spinner Loader
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 8000)
+    }, [])
+
     return (
         <Suspense fallback={<h1>Loading...</h1>}>
+            {loading?
+                <ClipLoader
+                    color={"#123abc"}
+                    loading={loading}
+                    size={30}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+                :
         <div>
             <CardsContainer />
-        </div>
+        </div>}
     </Suspense>)
 }
 
