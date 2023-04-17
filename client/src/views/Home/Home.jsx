e5af90e06e80082a70f31524a4ae9fc2529d860f
ajpintos <ajpintos@gmail.com>
2023-04-16 23:32:17 -0300

    lazy load

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,22 +1,26 @@
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
-import {useEffect} from "react";
+import {lazy, Suspense, useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {getCountries} from "../../redux/actions";
 import NavBar from "../../components/NavBar/NavBar";
 import App from "../../App";
 
+
+
+
+const Cc = lazy(() => import('../../components/CardsContainer/CardsContainer'));
 const Home = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
         dispatch(getCountries());
     },[dispatch])
-    return (
+    return (<Suspense fallback={<div>Loading...</div>}>
         <div>
         {/*<h1>Esta es la vista de Home</h1>*/}
             <CardsContainer />
         </div>
-    )
+    </Suspense>)
 }
 
 export default Home
